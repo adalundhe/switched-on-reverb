485ef8362307d3613b5589eb1752f19d6ce0e688
sscienceUM <sean.corbett@umontana.edu>
2022-08-21 14:30:32 -0500

    wip

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,15 +2,11 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
 import { listingsRouter } from './listings'
-import { protectedExampleRouter } from "./protected-example-router";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge('listings.', listingsRouter)
-  .merge("question.", protectedExampleRouter);
+  .merge('listings.', listingsRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
